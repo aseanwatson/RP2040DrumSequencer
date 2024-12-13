02b24d412dfb508bfacac8a3e9b6f70b39eb1270
Sean <aseanwatson@outlook.com>
2024-12-13 15:19:24 -0800

    documentation for drum class

--- drum.py
+++ drum.py
@@ -2,6 +2,7 @@ import usb_midi
 from bitarray import bitarray
 
 class drum:
+    """drum class: represents a drum and its beat sequence"""
     channel = 10
     velocity = 120
     note_on = 0x90
@@ -13,9 +14,11 @@ class drum:
         self.sequence = sequence
 
     def __repr__(self) -> str:
+        """__repr___(): used to supply the repr(...) function with a string which would recreate the drum."""
         return f'drum({repr(self.name)},{repr(self.note)},{repr(self.sequence)})'
 
     def play(self, midi: usb_midi.PortOut, step: int) -> None:
+        """play(midi, step): plays the drum's note, on the midi port, if the step is active."""
         if self.sequence[step]:
             channel_flag = drum.channel - 1 # 0 = ch 1, 1 = ch2, ..., 9 = ch 10
             midi_msg_on = bytearray([channel_flag | drum.note_on, self.note, drum.velocity])
