827f6d1febe1f62097f38fbaf0ce8d2a940ffcbb
Sean <aseanwatson@outlook.com>
2024-12-12 16:35:54 -0800

    Move hardware access to its own class

--- code.py
+++ code.py
@@ -26,61 +26,67 @@ from ticker import ticker
 
 ticker = ticker(120)
 
-# define I2C
-i2c = board.STEMMA_I2C()
+class hardware:
+    def __init__(self):
+        # define I2C
+        self.i2c = board.STEMMA_I2C()
+
+        # Start button
+        start_button_in = DigitalInOut(board.A2)
+        start_button_in.pull = Pull.UP
+        self.start_button = Debouncer(start_button_in)
+
+        # Reverse button
+        reverse_button_in = DigitalInOut(board.A1)
+        reverse_button_in.pull = Pull.UP
+        self.reverse_button = Debouncer(reverse_button_in)
+
+        # Setup switches
+        self.switches = keypad.ShiftRegisterKeys(
+            data = board.SCK,
+            latch = board.MOSI,
+            clock = board.MISO,
+            key_count = 40,
+            value_when_pressed = True,
+            value_to_latch = True,
+            )
+
+        # Setup LEDs
+        self.leds = TLC5916(
+            oe_pin = board.D5,
+            sdi_pin = board.D3,
+            clk_pin = board.D2,
+            le_pin = board.D4,
+            n = 5)
+
+        # STEMMA QT Rotary encoder setup
+        rotary_seesaw = seesaw.Seesaw(hardware.i2c, addr=0x36)  # default address is 0x36
+        self.encoder = rotaryio.IncrementalEncoder(rotary_seesaw)
+
+        rotary_seesaw.pin_mode(24, rotary_seesaw.INPUT_PULLUP)  # setup the button pin
+        knobbutton_in = digitalio.DigitalIO(rotary_seesaw, 24)  # use seesaw digitalio
+        self.knobbutton = Debouncer(knobbutton_in)  # create debouncer object for button
+
+        # MIDI setup
+        self.midi = usb_midi.ports[1]
+
+        # Display
+        self.display = segments.Seg14x4(hardware.i2c, address=(0x70))
+
+hardware = hardware()
 
 num_steps = 8  # number of steps/switches per row
 
 stepper = stepper(num_steps)
 playing = False
 
-# Setup button
-start_button_in = DigitalInOut(board.A2)
-start_button_in.pull = Pull.UP
-start_button = Debouncer(start_button_in)
+hardware.leds.write_config(0)
 
-# Reverse button
-reverse_button_in = DigitalInOut(board.A1)
-reverse_button_in.pull = Pull.UP
-reverse_button = Debouncer(reverse_button_in)
-
-# Setup switches
-# Input shift register
-switches = keypad.ShiftRegisterKeys(
-    data = board.SCK,
-    latch = board.MOSI,
-    clock = board.MISO,
-    key_count = 40,
-    value_when_pressed = True,
-    value_to_latch = True,
-    )
-
-
-# Setup LEDs
-# Output shift register
-leds = TLC5916(
-    oe_pin = board.D5,
-    sdi_pin = board.D3,
-    clk_pin = board.D2,
-    le_pin = board.D4,
-    n = 5)
-
-leds.write_config(0)
-#
-# STEMMA QT Rotary encoder setup
-rotary_seesaw = seesaw.Seesaw(i2c, addr=0x36)  # default address is 0x36
-encoder = rotaryio.IncrementalEncoder(rotary_seesaw)
 last_encoder_pos = 0
-rotary_seesaw.pin_mode(24, rotary_seesaw.INPUT_PULLUP)  # setup the button pin
-knobbutton_in = digitalio.DigitalIO(rotary_seesaw, 24)  # use seesaw digitalio
-knobbutton = Debouncer(knobbutton_in)  # create debouncer object for button
-encoder_pos = -encoder.position
-
-# MIDI setup
-midi = usb_midi.ports[1]
+encoder_pos = -hardware.encoder.position
 
 # default starting sequence
-drums = drum_set(midi, num_steps)
+drums = drum_set(hardware.midi, num_steps)
 drums.add_drum("Bass", 36)
 drums.add_drum("Snar", 38)
 drums.add_drum("LTom", 41)
@@ -89,11 +95,11 @@ drums.add_drum("HTom", 56)
 
 def light_steps(drum_index: int, step: int, state: bool):
     # pylint: disable=global-statement
-    global leds, num_steps
+    global hardware, num_steps
     remap = [4, 5, 6, 7, 0, 1, 2, 3]
     new_drum = 4 - drum_index
     new_step = remap[step]
-    leds[new_drum * num_steps + new_step] = state
+    hardware.leds[new_drum * num_steps + new_step] = state
     if state:
         print(f'drum{drum_index} step{step}: on')
     else:
@@ -154,37 +160,37 @@ def load_state() -> None:
 # try to load the state (no-op if NVM not valid)
 load_state()
 
-display = segments.Seg14x4(i2c, address=(0x70))
-display.brightness = 0.3
-display.fill(0)
-display.show()
-display.print(ticker.bpm)
-display.show()
+hardware.display.brightness = 0.3
+hardware.display.fill(0)
+hardware.display.show()
+hardware.display.print(ticker.bpm)
+hardware.display.show()
 
 print("Drum Trigger 2040")
 
 
-display.fill(0)
-display.show()
-display.marquee("Drum", 0.05, loop=False)
+hardware.display.fill(0)
+hardware.display.show()
+hardware.display.marquee("Drum", 0.05, loop=False)
 time.sleep(0.5)
-display.marquee("Trigger", 0.075, loop=False)
+hardware.display.marquee("Trigger", 0.075, loop=False)
 time.sleep(0.5)
-display.marquee("2040", 0.05, loop=False)
+hardware.display.marquee("2040", 0.05, loop=False)
 time.sleep(1)
-display.marquee("BPM", 0.05, loop=False)
+hardware.display.marquee("BPM", 0.05, loop=False)
 time.sleep(0.75)
-display.marquee(str(ticker.bpm), 0.1, loop=False)
+hardware.display.marquee(str(ticker.bpm), 0.1, loop=False)
 
 # light up initial LEDs
 for drum_index in range(len(drums)):
     drum = drums[drum_index]
     for step_index in range(num_steps):
         light_steps(drum_index, step_index, drum.sequence[step_index])
-leds.write()
+hardware.leds.write()
+
 while True:
-    start_button.update()
-    if start_button.fell:  # pushed encoder button plays/stops transport
+    hardware.start_button.update()
+    if hardware.start_button.fell:  # pushed encoder button plays/stops transport
         if playing is True:
             drums.print_sequence()
             save_state()
@@ -193,8 +199,8 @@ while True:
         ticker.restart()
         print("*** Play:", playing)
 
-    reverse_button.update()
-    if reverse_button.fell:
+    hardware.reverse_button.update()
+    if hardware.reverse_button.fell:
         stepper.reverse()
 
     if playing:
@@ -202,12 +208,12 @@ while True:
             # TODO: how to display the current step? Separate LED?
             drums.play_step(stepper.current_step)
             stepper.advance_step()
-            encoder_pos = -encoder.position  # only check encoder while playing between steps
+            encoder_pos = -hardware.encoder.position  # only check encoder while playing between steps
     else:  # check the encoder all the time when not playing
-        encoder_pos = -encoder.position
+        encoder_pos = -hardware.encoder.position
 
     # switches add or remove steps
-    switch = switches.events.get()
+    switch = hardware.switches.events.get()
     if switch:
         if switch.pressed:
             i = switch.key_number
@@ -217,13 +223,13 @@ while True:
             drum = drums[drum_index]
             drum.sequence.toggle(step_index) # toggle step
             light_steps(drum_index, step_index, drum.sequence[step_index])  # toggle light
-            leds.write()
+            hardware.leds.write()
 
     if encoder_pos != last_encoder_pos:
         encoder_delta = encoder_pos - last_encoder_pos
         ticker.adjust_bpm(encoder_delta)
-        display.fill(0)
-        display.print(ticker.bpm)
+        hardware.display.fill(0)
+        hardware.display.print(ticker.bpm)
         last_encoder_pos = encoder_pos
 
  # suppresions:
