8ceaa65ce3cda97b5ddddedadb3031a41dc3598b
Sean <aseanwatson@outlook.com>
2024-12-13 15:13:50 -0800

    use named constants in `drum` class

--- drum.py
+++ drum.py
@@ -2,6 +2,11 @@ import usb_midi
 from bitarray import bitarray
 
 class drum:
+    channel = 10
+    velocity = 120
+    note_on = 0x90
+    note_off = 0x80
+
     def __init__(self, name: str, note: int, sequence: bitarray):
         self.name = name
         self.note = note
@@ -12,8 +17,9 @@ class drum:
 
     def play(self, midi: usb_midi.PortOut, step: int) -> None:
         if self.sequence[step]:
-            midi_msg_on = bytearray([0x99, self.note, 120])  # 0x90 is noteon ch 1, 0x99 is noteon ch 10
-            midi_msg_off = bytearray([0x89, self.note, 0])
+            channel_flag = drum.channel - 1 # 0 = ch 1, 1 = ch2, ..., 9 = ch 10
+            midi_msg_on = bytearray([channel_flag | drum.note_on, self.note, drum.velocity])
+            midi_msg_off = bytearray([channel_flag | drum.note_off, self.note, 0])
             midi.write(midi_msg_on)
             midi.write(midi_msg_off)
 
