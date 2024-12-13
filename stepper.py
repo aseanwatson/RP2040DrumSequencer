080c78f4b773d271e4818759b3f03576e8d27a97
Sean <aseanwatson@outlook.com>
2024-12-13 15:08:03 -0800

    Add documentation for stepper class

--- stepper.py
+++ stepper.py
@@ -1,4 +1,5 @@
 class stepper:
+    """stepper class: Keeps track of which step we are on and what the range of steps are."""
     def __init__(self, num_steps: int):
         self.current_step = 0
         self.first_step = 0
@@ -7,6 +8,7 @@ class stepper:
         self.num_steps = num_steps
 
     def advance_step(self) -> int:
+        """advance_step(): moves to the next step (maybe backwards) and keeps the step in the allowed range."""
         if self.stepping_forward:
             if self.current_step < self.last_step:
                 self.current_step = self.current_step + 1
@@ -20,14 +22,17 @@ class stepper:
         return self.current_step
 
     def reverse(self) -> None:
+        """reverse(): changes the direction we're stepping forward <==> backward."""
         self.stepping_forward = not self.stepping_forward
 
     def reset(self) -> None:
+        """reset(): changes the direction we're stepping forward <==> backward."""
         self.current_step = self.first_step
 
     def adjust_range_start(self, adjustment: int) -> None:
-        # keep adjustment in the range where self.first_step >= 0 and
-        # self.last_step < self.num_steps
+        """adjust_range_start(adjustement): Updates the start of the range while keeping the number of steps constant.
+        
+        Ensures we keep the range valid; first_step can't be < 0 and last_step can't be >= num_steps."""
         adjustment = max(adjustment, -self.first_step)
         adjustment = min(adjustment, self.last_step - 1 - self.num_steps)
         self.first_step += adjustment
@@ -37,8 +42,9 @@ class stepper:
         # eventually. We might want to revisit this.
 
     def adjust_range_length(self, adjustment: int) -> None:
-        # keep adjustment in the range where self.first_step <= self.last_step and
-        # self.last_step < self.num_steps
+        """adjust_range_start(adjustement): Updates the length of the range while keeping the first steps constant.
+        
+        Ensures we keep the range valid; last_step can't be < first_step and last_step can't be >= num_steps."""
         adjustment = max(adjustment, self.first_step - self.last_step)
         adjustment = min(adjustment, self.last_step - 1 - self.num_steps)
         self.last_step += adjustment
