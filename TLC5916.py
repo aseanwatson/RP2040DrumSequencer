96363888062447ca9598e41261f57056be66607a
Sean <aseanwatson@outlook.com>
2024-12-15 04:04:26 -0800

    add current calculations for TLC5916

--- TLC5916.py
+++ TLC5916.py
@@ -12,13 +12,14 @@ class TLC5916:
         pin.direction = digitalio.Direction.OUTPUT
         return pin
 
-    def __init__(self, clk_pin, le_pin, sdi_pin, oe_pin, n):
+    def __init__(self, clk_pin, le_pin, sdi_pin, oe_pin, n, R_ext = None):
         self.ba = bytearray(n)
         self.clk = TLC5916.get_digital_out(clk_pin)
         self.le = TLC5916.get_digital_out(le_pin)
         self.sdi = TLC5916.get_digital_out(sdi_pin)
         self.oe = TLC5916.get_digital_out(oe_pin)
         self.oe.value = False
+        self.R_ext = R_ext
 
     def __setitem__(self, i, b):
         """__setitem__(i, b): Sets the i-th LED to on (if b is True) or off (if b is False)"""
@@ -53,7 +54,7 @@ class TLC5916:
         
         It sends bit 0 first (little endian)."""
         for i in range(8):
-            self.sdi.value = bool(byte & (1 << i))
+            self.sdi.value = bool(byte & (1 << (7-i)))
             self.pulse_clock()
 
     def write(self):
@@ -87,10 +88,13 @@ class TLC5916:
         See https://www.ti.com/lit/ds/symlink/tlc5916.pdf section 10.1.4 for details.
         The current is determined by this value and the value of the R_ext resistor.
 
-        For CM = 0; HC = 0:
+        I_out = (64 + D)/(R_ext * 40.6349) * (3 if CM else 1) * (2 if HC else 1)
 
-        I_out = (64+D)/(40.6349206349206 * R_ext)
+        So I_out is between 1.575/R_ext (D=0,CM=0,HC=0) and 18.7524/R_ext (D=63,CM=1,HC=1)
+
         where D = CC5 << 0 | CC4 << 1 | CC3 << 2 | CC2 << 3 | CC1 << 4 | CC0 << 5
+
+        D = I_out * R_ext / (40.6349 * (3 if CM else 1) * (2 if HC else 1)) - 64
 
         If set, HC multiplies the current by a factor of 2.
 
@@ -100,3 +104,57 @@ class TLC5916:
             self.write_byte(value)
         self.pulse_latch()
         self.set_special_mode(False)
+
+    @property
+    def min_current(self):
+        if self.R_ext == None:
+            raise ValueError('min_current; R_ext must be sepcified in constructor')
+        return 1.575 / self.R_ext
+
+    @property
+    def max_current(self):
+        if self.R_ext == None:
+            raise ValueError('min_current; R_ext must be sepcified in constructor')
+        return 18.7524 / self.R_ext
+
+    def set_target_current(self, value, *, R_ext = None, prefer_high_CM = False):
+        """set_target_current: computes a suitable value for write_config"""
+        effective_R_ext = self.R_ext if R_ext == None else R_ext
+        if effective_R_ext == None:
+            raise ValueError('set_target_current; R_ext must be sepcified in call or in constructor')
+
+        CM = 1 if prefer_high_CM else 0
+        if value < 0.010: # CM should only be 1 for 10mA-120mA
+            CM = 0
+        elif value > 0.040: # CM should only be 0 for 3mA-40mA
+            CM = 1
+
+        I_ref = value if CM == 0 else value / 3.0
+
+        HC = 0
+        if I_ref > 0.5:
+            I_ref /= 2.0
+            HC = 1
+
+        D = int(40.6349206349206 * effective_R_ext * I_ref) - 64
+
+        if D < 0:
+            raise ValueError(f'set_target_current({value}, {effective_R_ext}) cannot produce a current that low')
+
+        if D > 63:
+            raise ValueError(f'set_target_current({value}, {effective_R_ext}) cannot produce a current that high')
+
+        config_value = 0
+        if (D >> 4) & 1:
+            config_value |= 0b0000001
+        if (D >> 3) & 1:
+            config_value |= 0b0000010
+        if (D >> 2) & 1:
+            config_value |= 0b0000100
+        if (D >> 1) & 1:
+            config_value |= 0b0001000
+        if (D >> 0) & 1:
+            config_value |= 0b0010000
+        config_value |= HC << 6 | CM << 7
+
+        return self.write_config(config_value)
