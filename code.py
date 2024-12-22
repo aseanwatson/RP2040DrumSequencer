ecff44becb3db603a9b2c8c5ec24c66d67cb5671
Sean <aseanwatson@outlook.com>
2024-12-22 13:13:33 +1100

    move file_header class to its own file

--- code.py
+++ code.py
@@ -9,59 +9,12 @@
 # Range is note 35/B0 - 81/A4, but classic 808 set is defined here
 
 import stepper
-import struct
 import microcontroller
 import drum_set
 import ticker
 import hardware
 import relative_encoder
-
-class file_header:
-    """format of the header in NVM for save_state/load_state:
-        < -- little-endian; lower bits are more significant
-        B -- magic number
-        B -- number of drums (unsigned byte: 0 - 255)
-        B -- number of steps (unsigned byte: 0 - 255)
-        H -- BPM beats per minute (unsigned short: 0 - 65536)"""
-    format = b'<BBH'
-    """magic_number should change if load/save logic changes in and incompatible way"""
-    magic_number = 0x02
-    size = struct.calcsize(format)
-    def __init__(self, drum_count, step_count, bpm) -> None:
-        self.magic_number = file_header.magic_number
-        self.drum_count = drum_count
-        self.step_count = step_count
-        self.bpm = bpm
-
-    def get_save_length(self) -> int:
-        return file_header.size
-
-    def load_state_from_bytes(self, bytes, offset: int = 0) -> int:
-        index = offset
-        values = struct.unpack_from(file_header.format, buffer = bytes, offset = index)
-        index += file_header.size
-        # TODO: This should be a more specific exception
-        if self.magic_number != values[0]:
-            raise ValueError("bad magic number")
-        if self.drum_count != values[1]:
-            raise ValueError("bad drum_count")
-        if self.step_count != values[2]:
-            raise ValueError("bad step_count")
-        self.bpm = values[3]
-        return index - offset
-
-    def save_state_to_bytes(self, bytes, offset: int = 0) -> int:
-        index = offset
-        struct.pack_into(
-            file_header.format,
-            bytes,
-            index,
-            file_header.magic_number,
-            self.drum_count,
-            self.step_count,
-            self.bpm)
-        index += file_header.size
-        return index - offset
+import file_header
 
 class sequencer:
     def refresh_step_led(self, drum_index: int, step: int, state: bool):
@@ -80,7 +33,7 @@ class sequencer:
         self.hardware.display.show()
 
     def get_save_length(self) -> int:
-        length = file_header.size
+        length = file_header.file_header.size
         for drum in self.drums:
             length += drum.sequence.bytelen()
         return length
@@ -100,7 +53,7 @@ class sequencer:
         to store the state.
         """
         index = offset
-        header = file_header(
+        header = file_header.file_header(
             len(self.drums),
             self.stepper.num_steps,
             self.ticker.bpm)
@@ -120,7 +73,7 @@ class sequencer:
         to read the state.
         """
         index = offset
-        header = file_header(
+        header = file_header.file_header(
             len(self.drums),
             self.stepper.num_steps,
             self.ticker.bpm)
---
+++ file_header.py
@@ -0,0 +1,48 @@
+import struct
+class file_header:
+    """format of the header in NVM for save_state/load_state:
+        < -- little-endian; lower bits are more significant
+        B -- magic number
+        B -- number of drums (unsigned byte: 0 - 255)
+        B -- number of steps (unsigned byte: 0 - 255)
+        H -- BPM beats per minute (unsigned short: 0 - 65536)"""
+    format = b'<BBH'
+    """magic_number should change if load/save logic changes in and incompatible way"""
+    magic_number = 0x02
+    size = struct.calcsize(format)
+    def __init__(self, drum_count, step_count, bpm) -> None:
+        self.magic_number = file_header.magic_number
+        self.drum_count = drum_count
+        self.step_count = step_count
+        self.bpm = bpm
+
+    def get_save_length(self) -> int:
+        return file_header.size
+
+    def load_state_from_bytes(self, bytes, offset: int = 0) -> int:
+        index = offset
+        values = struct.unpack_from(file_header.format, buffer = bytes, offset = index)
+        index += file_header.size
+        # TODO: This should be a more specific exception
+        if self.magic_number != values[0]:
+            raise ValueError("bad magic number")
+        if self.drum_count != values[1]:
+            raise ValueError("bad drum_count")
+        if self.step_count != values[2]:
+            raise ValueError("bad step_count")
+        self.bpm = values[3]
+        return index - offset
+
+    def save_state_to_bytes(self, bytes, offset: int = 0) -> int:
+        index = offset
+        struct.pack_into(
+            file_header.format,
+            bytes,
+            index,
+            file_header.magic_number,
+            self.drum_count,
+            self.step_count,
+            self.bpm)
+        index += file_header.size
+        return index - offset
+
