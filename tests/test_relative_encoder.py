c91cc7cc09c834717d365288d84c4ccef8a1df29
Sean <aseanwatson@outlook.com>
2024-12-26 17:26:34 +1100

    test relative_encoder.py

--- tests/test_relative_encoder.py
+++ tests/test_relative_encoder.py
@@ -1,1 +1,12 @@
 from ..src import relative_encoder
+import rotaryio
+import board
+
+def test_relative_encoder():
+    enc = rotaryio.IncrementalEncoder(board.D1, board.D2)
+    enc.position = 10
+    re = relative_encoder.relative_encoder(enc)
+    assert re.read_value() == 0
+    enc.position = 11
+    assert re.read_value() == 1
+    assert re.read_value() == 0
